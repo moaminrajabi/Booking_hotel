92fb8341410e4a5c2c3355e4c151b1e2bd29197b
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-31 11:13:47 +0330

    add app layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import { Toaster } from "react-hot-toast";
 import LocationList from "./components/LocationList/LocationList";
 import { Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
       <Toaster />
       <Routes>
         <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}>
+          <Route index element={<div>hotels</div>} />
+          <Route path=":id" element={<div>single hotle</div>} />
+          <Route />
+        </Route>
       </Routes>
     </div>
   );
