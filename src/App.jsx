1f3df5a8439fbd5eec2d1f3c903208b33bfa65a7
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-31 11:29:00 +0330

    add Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Toaster } from "react-hot-toast";
 import LocationList from "./components/LocationList/LocationList";
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
+import Hotels from "./components/hotels/Hotels";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LocationList />} />
         <Route path="/hotels" element={<AppLayout />}>
-          <Route index element={<div>hotels</div>} />
+          <Route index element={<Hotels />} />
           <Route path=":id" element={<div>single hotle</div>} />
           <Route />
         </Route>
