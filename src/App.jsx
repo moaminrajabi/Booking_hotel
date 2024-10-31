e1aea4dabd9bb5e358634bd3c9ea753d5088e187
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-31 10:42:18 +0330

    add Location List components and useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import { Toaster } from "react-hot-toast";
+import LocationList from "./components/LocationList/LocationList";
 
 function App() {
   return (
     <div>
       <Header />
+      <Toaster />
+      <LocationList />
     </div>
   );
 }
