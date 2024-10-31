a3dd2453e0399a8b26593336aa3a9846008511cc
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-31 17:13:11 +0330

    add marker location on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHotel } from "../../context/HotelProvider";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
   const { isLoading, hotels } = useHotel();
   const [mapCenter, setMapCenter] = useState([51, 3]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  useEffect(() => {
+    if ((lat, lng)) setMapCenter([lat, lng]);
+  }, [lat, lng]);
 
   return (
     <div className="mapContainer">
@@ -12,21 +20,29 @@ function Map() {
         className="map"
         center={mapCenter}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapCenter}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <ChangeCenter position={mapCenter} />
+        {hotels.map((item) => (
+          <Marker key={item.div} position={[item.latitude, item.longitude]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
-      ,
     </div>
   );
 }
 
 export default Map;
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
