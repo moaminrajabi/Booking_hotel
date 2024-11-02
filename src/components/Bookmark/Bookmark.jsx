1d63871c46053ea5a5598e23277b1b170b65089e
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-11-02 19:54:34 +0330

    delete bookmark

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,28 +1,45 @@
-import React from "react";
-import { useBookmark } from "../../context/BookmarkListContext";
-import Loader from "../Laader/Loader";
 import ReactCountryFlag from "react-country-flag";
+import Loader from "../Laader/Loader";
+import { useBookmark } from "../../context/BookmarkListContext";
 import { Link } from "react-router-dom";
+import { HiTrash } from "react-icons/hi";
 
-function BookMark() {
-  const { isLoading, bookmarks } = useBookmark();
+function Bookmark() {
+  const { isLoading, bookmarks, currentBookmark, deleteBookmark } =
+    useBookmark();
+
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    await deleteBookmark(id);
+  };
 
   if (isLoading) return <Loader />;
-  console.log(bookmarks);
+  if (!bookmarks.length) return <p>there is no bookmarked location</p>;
+
   return (
     <div>
-      <h2>Bookmark List</h2>
+      <h2>BookmarkList</h2>
+
       <div className="bookmarkList">
         {bookmarks.map((item) => {
           return (
             <Link
               key={item.id}
-              to={`${item.id}?lat=${item.latitude}?lan=${item.longitude}`}
+              to={`${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
             >
-              <div key={item.id} className="bookmarkItem">
-                <ReactCountryFlag svg countryCode={item.countryCode} /> &nbsp;
-                <strong> {item.cityName} </strong> &nbsp;
-                <span>{item.country}</span>
+              <div
+                className={`bookmarkItem ${
+                  item.id === currentBookmark?.id ? "current-bookmark" : ""
+                }`}
+              >
+                <div>
+                  <ReactCountryFlag svg countryCode={item.countryCode} />
+                  &nbsp; <strong>{item.cityName}</strong> &nbsp;
+                  <span>{item.country}</span>
+                </div>
+                <button onClick={(e) => handleDelete(e, item.id)}>
+                  <HiTrash className="trash" />
+                </button>
               </div>
             </Link>
           );
@@ -31,5 +48,4 @@ function BookMark() {
     </div>
   );
 }
-
-export default BookMark;
+export default Bookmark;
